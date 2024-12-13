4f33c5329d6c8e5aba4d3c2876b887d0461332c8
maopro825 <trungprodiepvien@gmail.com>
2024-12-13 13:38:54 +0700

    ADD UI

--- test-lab2.1/test-lab2.1/Form1.Designer.cs
+++ test-lab2.1/test-lab2.1/Form1.Designer.cs
@@ -115,7 +115,6 @@
             this.btnCong.TabIndex = 12;
             this.btnCong.Text = "+";
             this.btnCong.UseVisualStyleBackColor = true;
-            //this.btnCong.Click += new System.EventHandler(this.btnCong_Click);
             // 
             // txtNum1
             // 
@@ -152,6 +151,7 @@
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
 
